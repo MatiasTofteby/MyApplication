0a487621c662d56115a6b068f8ea90d21c20f03e
Matias Tofteby Bach Andersen <MatiasTofteby@Matiass-MacBook-Pro.local>
2015-06-07 17:51:36 +0200

    Initial commit

---
+++ app/src/main/java/com/matias/matiastofteby/myapplication/NyAppFragment.java
@@ -0,0 +1,23 @@
+package com.matias.matiastofteby.myapplication;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class NyAppFragment extends Fragment {
+
+    public NyAppFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_ny_app, container, false);
+    }
+}
